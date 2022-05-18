fbee3455eba6d8ba6e1e8c55548b165039d0efd8
I-Zafirov <ivoomail@gmail.com>
2022-05-18 16:07:36 +0300

    commit

---
+++ src/T03StacksAndQueues/lab/L4MatchingBrackets.java
@@ -0,0 +1,27 @@
+package T03StacksAndQueues.lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class L4MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+
+        ArrayDeque<Integer> openIndexes = new ArrayDeque<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char currentSymbol = expression.charAt(i);
+
+            if (currentSymbol == '(') {
+                openIndexes.push(i);
+            } else if (currentSymbol == ')') {
+                int lastOpenIndex = openIndexes.pop();
+                String matching = expression.substring(lastOpenIndex, i + 1);
+                System.out.println(matching);
+            }
+        }
+    }
+}
+
