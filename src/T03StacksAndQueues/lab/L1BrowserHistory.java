4a41a23e0dbf97cba3b0735068a54efdef3f5c79
I-Zafirov <ivoomail@gmail.com>
2022-05-18 15:35:32 +0300

    commit

--- src/T03StacksAndQueues/lab/L1BrowserHistory.java
+++ src/T03StacksAndQueues/lab/L1BrowserHistory.java
@@ -1,4 +1,37 @@
 package T03StacksAndQueues.lab;
 
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class L1BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nextNavigation = scanner.nextLine();
+
+        String currentURL = "blank";
+
+        ArrayDeque<String> history = new ArrayDeque<>();
+
+        while (!nextNavigation.equals("Home")) {
+
+            if (nextNavigation.equals("back")) {
+                if (!history.isEmpty()) {
+                    currentURL = history.pop();
+                } else {
+                    System.out.println("no previous URLs");
+                    nextNavigation = scanner.nextLine();
+                    continue;
+                }
+            } else {
+                if (!currentURL.equals("blank")) {
+                    history.push(currentURL);
+                }
+                currentURL = nextNavigation;
+            }
+            System.out.println(currentURL);
+            nextNavigation = scanner.nextLine();
+        }
+    }
 }
+
