b210f832b0706a568606e987715c24317751f9c4
I-Zafirov <ivoomail@gmail.com>
2022-05-19 11:28:16 +0300

    commit

---
+++ src/T03StacksAndQueues/lab/L3DecimalToBinary.java
@@ -0,0 +1,22 @@
+package T03StacksAndQueues.lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class L3DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        while (number != 0) {
+            stack.push(number % 2);
+            number /= 2;
+        }
+
+        if (stack.size() == 0) System.out.print(0);
+        else stack.forEach(System.out::print);
+    }
+}
