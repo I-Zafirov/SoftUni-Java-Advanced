de6163878095852ca4bf46876458ebe4ed382a9d
I-Zafirov <ivoomail@gmail.com>
2022-05-24 11:52:08 +0300

    commit

---
+++ src/T05MultidimensionalArrays/lab/L2PositionsOf3.java
@@ -0,0 +1,48 @@
+package T05MultidimensionalArrays.lab;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L2PositionsOf3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] matrix = readMatrix(scanner);
+        int number = (Integer.parseInt(scanner.nextLine()));
+
+        ArrayList<String> outputLines = new ArrayList<>();
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == number) {
+                    outputLines.add(row + " " + col);
+                }
+            }
+        }
+        if (outputLines.isEmpty()) {
+            System.out.println("not found");
+        } else {
+            for (String outputLine : outputLines) {
+                System.out.println(outputLine);
+            }
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        String[] dimensions = scanner.nextLine().split("\\s+");
+        int rows = Integer.parseInt(dimensions[0]);
+        int cols = Integer.parseInt(dimensions[1]);
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)//.mapToInt(e -> Integer.parseInt(e))
+                    .toArray();
+            matrix[row] = arr;
+        }
+
+        return matrix;
+    }
+}
