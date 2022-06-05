2af14f398a15bf15f909586b94e21da29f45a789
I-Zafirov <ivoomail@gmail.com>
2022-06-05 17:23:02 +0300

    commit

---
+++ src/T09StreamsFilesAndDirectories/exercise/Course.java
@@ -0,0 +1,9 @@
+package T09StreamsFilesAndDirectories.exercise;
+
+import java.io.*;
+
+    public class Course implements Serializable {
+        String name;
+        int numberOfStudents;
+    }
+
