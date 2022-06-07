db766a3f124d6379db42a5872d6ee37d895508d1
I-Zafirov <ivoomail@gmail.com>
2022-06-07 10:49:06 +0300

    commit

---
+++ src/T11FunctionalProgramming/exercise/E05ReverseAndExclude.java
@@ -0,0 +1,25 @@
+package T11FunctionalProgramming.exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class E05ReverseAndExclude {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Predicate<Integer> predicate = number -> number % n == 0;
+        numbers.removeIf(predicate);
+        Collections.reverse(numbers);
+        numbers.forEach(number -> System.out.print(number + " "));
+    }
+}
