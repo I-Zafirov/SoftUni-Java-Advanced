ca5375a271d088b9865ed3080bbc66b7a2151a9a
I-Zafirov <ivoomail@gmail.com>
2022-06-15 12:06:55 +0300

    commit

---
+++ src/T13DefiningClasses/exercise/E09CatLady/StreetExtraordinaire.java
@@ -0,0 +1,16 @@
+package T13DefiningClasses.exercise.E09CatLady;
+
+public class StreetExtraordinaire extends Cat {
+
+    private final double decibelsOfMeows;
+
+    public StreetExtraordinaire(String name, String breed, double decibelsOfMeows) {
+        super(name, breed);
+        this.decibelsOfMeows = decibelsOfMeows;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %.2f", getBreed(), getName(), this.decibelsOfMeows);
+    }
+}
