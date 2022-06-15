01e021f03ace513deb4a610c02a35a123b30c299
I-Zafirov <ivoomail@gmail.com>
2022-06-15 12:04:35 +0300

    commit

---
+++ src/T13DefiningClasses/exercise/E09CatLady/Cat.java
@@ -0,0 +1,24 @@
+package T13DefiningClasses.exercise.E09CatLady;
+
+public class Cat {
+    private final String name;
+    private final String breed;
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.breed, this.name);
+    }
+}
