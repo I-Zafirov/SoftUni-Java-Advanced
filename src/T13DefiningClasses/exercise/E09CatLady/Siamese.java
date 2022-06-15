326e0831ddf54e454ff98946152a57acc00d3b73
I-Zafirov <ivoomail@gmail.com>
2022-06-15 12:06:44 +0300

    commit

---
+++ src/T13DefiningClasses/exercise/E09CatLady/Siamese.java
@@ -0,0 +1,16 @@
+package T13DefiningClasses.exercise.E09CatLady;
+
+public class Siamese extends Cat {
+
+    private final double earSize;
+
+    public Siamese(String name, String breed, double earSize) {
+        super(name, breed);
+        this.earSize = earSize;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %.2f", getBreed(), getName(), this.earSize);
+    }
+}
