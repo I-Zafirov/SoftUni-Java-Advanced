0a292f7c642a85be027a713827e22770db272c44
I-Zafirov <ivoomail@gmail.com>
2022-06-17 11:11:10 +0300

    commit

---
+++ src/T17Generics/lab/L03GenericScale/Main.java
@@ -0,0 +1,10 @@
+package T17Generics.lab.L03GenericScale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scale<String> scale = new Scale<>("A", "B");
+
+        System.out.println(scale.getHeavier());
+    }
+}
