6bbcd2f7f5978f46226a16d44387c0fb2b7c5b23
I-Zafirov <ivoomail@gmail.com>
2022-06-17 11:13:27 +0300

    commit

---
+++ src/T17Generics/exercise/E02GenericBoxOfInteger/Box.java
@@ -0,0 +1,26 @@
+package T17Generics.exercise.E02GenericBoxOfInteger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> { //T -> String
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        values.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (T value : values) {
+            stringBuilder.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+
+        return stringBuilder.toString();
+    }
+}
