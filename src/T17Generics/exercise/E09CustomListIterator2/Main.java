d0cf000b28789d75ef5b41fc6e0d541f1b3e0319
I-Zafirov <ivoomail@gmail.com>
2022-06-19 12:26:12 +0300

    commit

---
+++ src/T17Generics/exercise/E09CustomListIterator2/Main.java
@@ -0,0 +1,47 @@
+package T17Generics.exercise.E09CustomListIterator2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Box<String> boxList = new Box<>();
+        String tokens;
+
+        while (!"END".equals(tokens = scanner.nextLine())) {
+            String[] token = tokens.split("\\s+");
+            switch (token[0]) {
+                case "Add":
+                    boxList.add(token[1]);
+                    break;
+                case "Remove":
+                    boxList.remove(Integer.parseInt(token[1]));
+                    break;
+                case "Contains":
+                    System.out.println(boxList.contains(token[1]));
+                    break;
+                case "Swap":
+                    boxList.swap(Integer.parseInt(token[1]), Integer.parseInt(token[2]));
+                    break;
+                case "Greater":
+                    System.out.println(boxList.countGreaterThan(token[1]));
+                    break;
+                case "Max":
+                    System.out.println(boxList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(boxList.getMin());
+                    break;
+                case "Sort":
+                    Sorter.sort(boxList);
+                    break;
+                case "Print":
+                    for (String s : boxList) {
+                        System.out.println(s);
+                    }
+                    break;
+            }
+        }
+    }
+}
