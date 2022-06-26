a3afa0c5bd47e52d6e26ec2308696330f956bb7a
I-Zafirov <ivoomail@gmail.com>
2022-06-26 11:17:05 +0300

    commit

---
+++ src/T23WorkshopAlgorithms/L07BinarySearch.java
@@ -0,0 +1,32 @@
+package T23WorkshopAlgorithms;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L07BinarySearch {
+    public static void main(String[] args) {
+        // yes Complexity O(log(n))
+        Scanner scanner = new Scanner(System.in);
+        int[] array = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int key = Integer.parseInt(scanner.nextLine());
+
+
+        System.out.println(getIndex(array, key, 0, array.length - 1));
+    }
+
+    private static int getIndex(int[] array, int key, int l, int r) {
+        while (l <= r) {
+            int mid = (r + l) / 2;
+            if (array[mid] > key) {
+                r = mid - 1;
+            } else if (array[mid] < key) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
